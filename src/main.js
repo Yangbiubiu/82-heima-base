a5e2ae1a8af6416c447a421922d7e40ea05a0f03
yangyuwei <627888464@qq.com>
2019-08-31 21:13:28 +0800

    整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'  // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-Vue.use(ElementUI)// 全局注册elementUI组件 任意位置都可以使用
-Vue.config.productionTip = false
+import './style/index.less' // 引入初始化样式 包括 margin padding li
 
+Vue.config.productionTip = false
+Vue.use(ElementUI)// 全局注册elementUI组件 任意位置都可以使用
 new Vue({
   router,
   render: h => h(App)
