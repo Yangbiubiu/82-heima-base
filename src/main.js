72995b9c92d98a9cbb1767866d16ea21e6fd3ce1
yangyuwei <627888464@qq.com>
2019-08-31 21:07:36 +0800

    引入elementui组件和样式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'  // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+Vue.use(ElementUI)// 全局注册elementUI组件 任意位置都可以使用
 Vue.config.productionTip = false
 
 new Vue({
