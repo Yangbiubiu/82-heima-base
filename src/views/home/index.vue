9288a21fac1fcb0a67608af12a287aacad35cd77
yangyuwei <627888464@qq.com>
2019-09-02 16:35:11 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class='main'>
-      home1
-  </div>
+  <!-- 最外层容器 -->
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside style="'width:200px;background-color:#323745">左侧</el-aside>
+    <!-- 右侧 -->
+      <el-container>
+      <!-- 头部 -->
+        <el-header>头部</el-header>
+      <!-- 主要区域 -->
+        <el-main style='margin:0;padding:0'>主体</el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
