a072de6bef2673a33be33e2a3356c2dc516836e9
yangyuwei <627888464@qq.com>
2019-09-02 17:23:08 +0800

    主页模块-左侧导航菜单引用组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,19 +2,35 @@
   <!-- 最外层容器 -->
   <el-container>
     <!-- 左侧导航 -->
-    <el-aside style="'width:200px;background-color:#323745">左侧</el-aside>
+    <el-aside style='width:200px;background-color:#323745'>
+      <layout-aside></layout-aside>
+    </el-aside>
     <!-- 右侧 -->
-      <el-container>
+    <el-container>
       <!-- 头部 -->
-        <el-header>头部</el-header>
+      <el-header>
+        <!-- 头部组件 -->
+        头部
+      </el-header>
       <!-- 主要区域 -->
-        <el-main style='margin:0;padding:0'>主体</el-main>
-      </el-container>
+      <el-main style='margin:0;padding:0'>
+       主体
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+
+//1.引入组件
+import layoutAside from '../../components/home/layout-aside.vue'
+
+export default {
+  //2.注册组件
+  components:{
+    'layout-aside':layoutAside
+  }
+}
 </script>
 
 <style>
