33a1ac8015a17150b368a6d1584dabb1dff687ba
yangyuwei <627888464@qq.com>
2019-09-03 20:39:14 +0800

    头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -14,7 +14,7 @@
   -->
       <img class="head-img" :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
 <!-- trigger elementUI组件自带 -->
-      <el-dropdown trigger="click">
+      <el-dropdown trigger="click" @command="handleMenuItem">
         <!-- 匿名插槽 -->
         <span class="el-dropdown-link">
           {{userInfo.name}}
@@ -23,9 +23,10 @@
 
         <!-- 具名插槽 -->
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item >个人信息</el-dropdown-item>
-          <el-dropdown-item >git地址</el-dropdown-item>
-          <el-dropdown-item >退出</el-dropdown-item>
+           <!-- command='xxx' 为了分辨点击了哪个-->
+        <el-dropdown-item command='account'>个人信息</el-dropdown-item>
+        <el-dropdown-item command='git'>git地址</el-dropdown-item>
+        <el-dropdown-item command='lgout'>退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -53,16 +54,30 @@ export default {
         console.log(result)
         this.userInfo = result.data.data//获取用户数据并赋值给空对象
       })
+    },  
+      // 跳转到登录页=>成功的标志是token=>退出的标志是清除token
+      // 监听 el-dropdown中的command事件,还要给el-dropdown-item 一个command属性
+  // 点击菜单项触发的方法/事件 用户可以通过相应的菜单项key(自定属性名)进行不同的操作
+    handleMenuItem (command) {//固定写法
+    // alert(command)
+    if (command === 'account') {//command='xxx' 为了分辨点击了哪个 xxx为自定属性名
+        // 账户信息
+      } else if (command === 'git') {
+        // git地址
+        window.location.href = 'https://github.com/shuiruohanyu/82heimatoutiao' // 跳转到xxx页面 改变当前的地址
+      } else {
+        // 退出  
+        window.localStorage.clear() // 点击退出可清除token缓存 退出的标志是清除token
+        this.$router.push('/login') // 跳转到登录页
+       
+
+      }
+      // console.log(command)
     }
   },
   created () {
     this.getUserInfo()
-  },
-    // 点击菜单项触发的方法
-    handleMenuItem(){
-
-    }
-
+  } 
 }
 </script>
 
