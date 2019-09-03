44fadb345acb71135ceef05fe25aa5b7d5a2a952
yangyuwei <627888464@qq.com>
2019-09-03 22:06:52 +0800

    默认导航

--- src/router.js
+++ src/router.js
@@ -2,23 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home'
 import Login from './views/login'
+import Main from './views/home/main'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes: [// 配置路由表
     {
       path: '/login',
       component: Login
     },
     {
       path: '/',
-      redirect: '/home'// 一登陆默认进入home页 
-    
+      redirect: '/home'
+      // 一登陆默认进入home页
     },
     {
       path: '/home', // views里先配置好<router-view> 在这引入保存views路径
       name: 'home',
-      component: Home
+      component: Home,
+      children: [{
+        // 二级路由的地址如果以斜杠‘/’开头,就会从根上计算你的路径 （例如: /a=>路径为 /a）
+        // 如果不以斜杠开头就会从你的父路由上进行追加 默认二级路由的简写就是什么也不写
+        path: '', // 默认的二级路由的地址 path为空串 用了嵌套路由
+        component: Main
+      }]
     }
     // {
     //   path: '/about',
---
+++ src/views/home/main.vue
@@ -0,0 +1,18 @@
+<template>
+<!-- 当作home主页的默认内容  定义好后挂在router.js路由上使用-->
+  <div class='main'></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+ .main {
+     height:100vh;
+     background-image: url('../../assets/img/home.jpg');
+     background-size: cover;
+ }
+</style>
